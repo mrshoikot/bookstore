8ddbec2a63ceed505c5cbfe4481fe544218f5bdb
mrshoikot <mrshoikot@gmail.com>
2022-11-15 00:09:45 +0600

    Updated map coordinate

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Kagoz</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -536,7 +536,7 @@
             <div class="container">
               <p>
                 &copy; <span id="displayYear"></span> All Rights Reserved By
-                <a href="https://html.design/">Free Html Templates</a>
+                <a href="#">KagoZ LLC</a>
               </p>
             </div>
           </footer>
