00c070a298b39b199b09f4864816e835f4831461
mrshoikot <mrshoikot@gmail.com>
2022-11-20 22:34:38 +0600

    fixed description

--- resources/views/book-edit.blade.php
+++ resources/views/book-edit.blade.php
@@ -97,7 +97,7 @@
                             </label>
                           </div>
                           <div class="md:w-2/3">
-                            <textarea value={{$book->description}} name="description" class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-password" type="text" ></textarea>
+                            <textarea name="description" class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-password" type="text" >{{$book->description}}</textarea>
                           </div>
                         </div>
                         
